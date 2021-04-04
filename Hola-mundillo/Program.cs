354c442c35c6cf4aad64225ef64b90eca7ff5116
JORGE <jorge.mendoza@pucp.edu.pe>
2021-04-04 08:59:22 -0500

    puse nombre cambio 2

--- Hola-mundillo/Program.cs
+++ Hola-mundillo/Program.cs
@@ -6,7 +6,7 @@ namespace Hola_mundillo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello cambio 2 !");
 
         }
     }
