7758a0244f61d3d51f5e4f607a1de7cf0e96f5ae
JORGE <jorge.mendoza@pucp.edu.pe>
2021-04-03 20:14:28 -0500

    Creacion del PROYECTO

---
+++ Hola-mundillo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hola_mundillo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
